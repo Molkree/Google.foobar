f5ee64fcc8e001efebb136effb66e35515052b08
Dmitry Volodin <mr.molkree@gmail.com>
2021-10-24 03:00:45 +0300

    7. WIP

--- 7_bringing_a_gun_to_a_trainer_fight.py
+++ 7_bringing_a_gun_to_a_trainer_fight.py
@@ -11,10 +11,79 @@
 
 # For example, if you and the elite trainer were positioned in a room with dimensions [3, 2], your_position [1, 1], trainer_position [2, 1], and a maximum shot distance of 4, you could shoot in seven different directions to hit the elite trainer (given as vector bearings from your location): [1, 0], [1, 2], [1, -2], [3, 2], [3, -2], [-3, 2], and [-3, -2]. As specific examples, the shot at bearing [1, 0] is the straight line horizontal shot of distance 1, the shot at bearing [-3, -2] bounces off the left wall and then the bottom wall before hitting the elite trainer with a total shot distance of sqrt(13), and the shot at bearing [1, 2] bounces off just the top wall before hitting the elite trainer with a total shot distance of sqrt(5).
 
+import math
+
+
+def get_distance(point_1, point_2):
+    x_1, y_1 = point_1
+    x_2, y_2 = point_2
+    return math.sqrt((x_2 - x_1) ** 2 + (y_2 - y_1) ** 2)
+
+
+def mirror_in_1st_quadrant(dimensions, your_position, trainer_position, distance):
+    width, height = dimensions
+    your_position_x, your_position_y = your_position
+    trainer_position_x, trainer_position_y = trainer_position
+    max_x = your_position_x + distance
+    max_y = your_position_y + distance
+    mirrors_x = max_x // width
+    mirrors_y = max_y // height
+    mirrored_points = []
+    for i in range(mirrors_x + 1):
+        for j in range(mirrors_y + 1):
+            mirrored_points.append(
+                [
+                    trainer_position_x + 2 * i * (width - trainer_position_x),
+                    trainer_position_y + 2 * j * (height - trainer_position_y),
+                    False,
+                ]
+            )
+            # need to include mirrors of your position to not shoot yourself
+            # but don't include yourself
+            if i or j:
+                mirrored_points.append(
+                    [
+                        your_position_x + 2 * i * (width - your_position_x),
+                        your_position_y + 2 * j * (height - your_position_y),
+                        True,
+                    ]
+                )
+    return mirrored_points
+
 
 def solution(dimensions, your_position, trainer_position, distance):
-    return 0
+    mirrored_points = mirror_in_1st_quadrant(
+        dimensions, your_position, trainer_position, distance
+    )
+    mirrored_points += (
+        # second quadrant
+        [[-point[0], point[1], point[2]] for point in mirrored_points]
+        # third quadrant
+        + [[-point[0], -point[1], point[2]] for point in mirrored_points]
+        # fourth quadrant
+        + [[point[0], -point[1], point[2]] for point in mirrored_points]
+    )
+    mirrored_points = [
+        point
+        for point in mirrored_points
+        if get_distance(your_position, [point[0], point[1]]) <= distance
+    ]
+    angles = set()
+    your_position_x, your_position_y = your_position
+    death_angles = set()
+    for beam in mirrored_points:
+        x, y, _ = beam
+        if [x, y] == your_position:
+            continue
+        angle = math.atan2(y - your_position_y, x - your_position_x)
+        angles.add(angle)
+        if beam[2]:
+            death_angles.add(angle)
+
+    angles -= death_angles
+
+    return len(angles)
 
 
 assert solution([3, 2], [1, 1], [2, 1], 4) == 7
-assert solution([300, 275], [150, 150], [185, 100], 500)
+assert solution([300, 275], [150, 150], [185, 100], 500) == 9
